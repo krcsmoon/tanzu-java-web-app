0bc4bf50f90cc833421586709598d3d3e9e7f5be
krcsmoon <csmoon@goodmit.co.kr>
2023-03-23 13:04:23 +0900

    tanzu-java-web-app commit

---
+++ src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		String str = "Greetings from Spring Boot + Tanzu! Welcome!";
+		return str;
+	}
+
+}
